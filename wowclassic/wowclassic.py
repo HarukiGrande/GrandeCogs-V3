07f67d1d40e4827d3e43364e2b02056efdaac63f
Haruki N. Grande <me@harukigrande.com>
2020-11-24 02:10:40 +0000

    Update wowclassic.py

--- wowclassic/wowclassic.py
+++ wowclassic/wowclassic.py
@@ -33,7 +33,7 @@ class WowClassic(BaseCog):
                 data = json.loads(f.read())
                 for item in data:
                     data_names.append(item["name"])
-                return await ctx.send(process.extractOne(query, data_names).index())
+                return await ctx.send(process.extractOne(query, data_names)[0])
         except FileNotFoundError:
             return await ctx.send("oops")
         await ctx.send(f"Could not find {query}.") 
