b686a8abf3f03d9acec4ffb3a140585e3a8129f0
Haruki N. Grande <me@harukigrande.com>
2020-11-24 01:57:14 +0000

    Update __init__.py

--- wowclassic/__init__.py
+++ wowclassic/__init__.py
@@ -1,4 +1,6 @@
 from .wowclassic import WowClassic
+from redbot.core import data_manager
 
 def setup(bot):
+    data_manager.load_bundled_data(WowClassic(bot), __file__)
     bot.add_cog(WowClassic(bot))
