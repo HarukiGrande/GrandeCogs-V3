8985fd9547d7ac8a35fc7a12f8cd5741f8d61907
Haruki N. Grande <me@harukigrande.com>
2020-12-01 23:56:04 +0000

    Update wowclassic.py

--- wowclassic/wowclassic.py
+++ wowclassic/wowclassic.py
@@ -18,6 +18,7 @@ class WowClassic(BaseCog):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--incognito")
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument("window-size=1920x1080")
         chrome_options.add_argument("--ignore-certificate-errors")
