5e83734a2c12f45383f43fa5ecae71a3495124ad
Haruki N. Grande <me@harukigrande.com>
2020-12-04 00:56:33 +0000

    Update wowclassic.py

--- wowclassic/wowclassic.py
+++ wowclassic/wowclassic.py
@@ -18,10 +18,13 @@ class WowClassic(BaseCog):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--incognito")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--start-maximized")
+        chrome_options.addArguments("start-maximized")
+        chrome_options.addArguments("disable-infobars")
+        chrome_options.addArguments("--disable-extensions")
+        chrome_options.addArguments("--disable-dev-shm-usage")
         chrome_options.add_argument("window-size=1920x1080")
         chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.addArguments("--no-sandbox")
         self.driver = webdriver.Chrome(options=chrome_options, executable_path=binary_path)
 
     def cog_unload(self):
